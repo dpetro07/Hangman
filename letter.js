f2550aeeecd9baa9fdc4dfe04205b6caf80ab0fd
Daniel Petro <DanielPetro@nbp-235-113.nbp.ruw.rutgers.edu>
2016-01-25 16:36:16 -0500

    created letter.js function and made two other js files called word.js and main.js

---
+++ letter.js
@@ -0,0 +1,14 @@
+var Letter = function(let) {
+  this.charac = let;
+  this.appear = false;
+
+  this.letterRender = function() {
+    if (this.appear === false){
+      return "_";
+    } else{
+      return this.charac;
+    }
+  }
+}
+
+module.exports = Letter;
